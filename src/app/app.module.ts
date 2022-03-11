ef76f8e91a6c1530d3fa4a5f1714982495b7ce39
Maria Brock <brockm@ornl.giv>
2022-03-11 10:33:44 -0600

    added additional code for examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,31 @@
-
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CoursesComponent } from './courses/courses.component';
-import { HomeComponent } from './home/home.component';
-import { MaterialModule } from './material.module';
-import { CoursesListComponent } from './courses/courses-list/courses-list.component';
-import { CourseDetailsComponent } from './courses/course-details/course-details.component';
-import { AdminComponent } from './examples/child-routes/admin/admin.component';
 import { AdminPayrollComponent } from './examples/child-routes/admin/admin-payroll/admin-payroll.component';
 import { AdminVacationComponent } from './examples/child-routes/admin/admin-vacation/admin-vacation.component';
+import { AdminComponent } from './examples/child-routes/admin/admin.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CourseDetailsComponent } from './courses/course-details/course-details.component';
+import { CoursesListComponent } from './courses/courses-list/courses-list.component';
+import { CoursesComponent } from './courses/courses.component';
+import { DynamicComponent } from './examples/dynamic-component/dynamic-component.component';
+import { ExamplesComponent } from './examples/examples.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { MaterialModule } from './material.module';
+import { AuthService } from './shared/services/auth/auth.service';
+import { NotificationService } from './shared/services/notifications/notification.service';
+import { CircleComponent } from './examples/dynamic-component/circle/circle.component';
+import { SquareComponent } from './examples/dynamic-component/square/square.component';
+import { TriangleComponent } from './examples/dynamic-component/triangle/triangle.component';
 import { CustomInputComponent } from './examples/custom-input/custom-input.component';
 import { CustomControlComponent } from './examples/custom-input/custom-control/custom-control.component';
-import { DynamicComponentComponent } from './examples/dynamic-component/dynamic-component.component';
-import { CircleModule } from './examples/dynamic-component/circle/circle.module';
-import { SquareModule } from './examples/dynamic-component/square/square.module';
-import { TriangleModule } from './examples/dynamic-component/triangle/triangle.module';
-import { LazyModuleComponent } from './examples/lazy-module/lazy-module.component';
-import { LazyRoutingModule } from './examples/lazy-module/lazy-routing/lazy-routing.module';
-import { ProtectedComponent } from './examples/protected/protected.component';
 import { RouteParamsComponent } from './examples/route-params/route-params.component';
-import { LoginComponent } from './login/login.component';
+import { ProtectedComponent } from './examples/protected/protected.component';
 
 @NgModule({
   imports: [
@@ -35,29 +35,28 @@ import { LoginComponent } from './login/login.component';
     FormsModule,
     MaterialModule,
     HttpClientModule,
-    CircleModule,
-    SquareModule,
-    TriangleModule,
-    LazyRoutingModule,
   ],
   declarations: [
     AppComponent,
     HomeComponent,
     CoursesComponent,
-    CoursesListComponent,
     CourseDetailsComponent,
+    CoursesListComponent,
+    LoginComponent,
     AdminComponent,
     AdminPayrollComponent,
     AdminVacationComponent,
+    ExamplesComponent,
+    DynamicComponent,
+    CircleComponent,
+    SquareComponent,
+    TriangleComponent,
     CustomInputComponent,
     CustomControlComponent,
-    DynamicComponentComponent,
-    LazyModuleComponent,
-    ProtectedComponent,
     RouteParamsComponent,
-    LoginComponent
+    ProtectedComponent,
   ],
-  providers: [],
+  providers: [AuthService, NotificationService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/examples/route-params/route-params.component.ts
+++ src/app/examples/route-params/route-params.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-route-params',
   templateUrl: './route-params.component.html',
-  styleUrls: ['./route-params.component.scss']
+  styleUrls: ['./route-params.component.scss'],
 })
 export class RouteParamsComponent implements OnInit {
+  currentId = null;
+  currentLesson = null;
 
-  constructor() { }
+  lessons = [
+    { id: '1', title: 'Hello Angular' },
+    { id: '2', title: 'Component Fundamentals' },
+    { id: '3', title: 'Template Driven Forms' },
+    { id: '4', title: 'Angular Services' },
+    { id: '5', title: 'Server Communication' },
+    { id: '6', title: 'Component Driven Architecture' },
+    { id: '7', title: 'Angular Routing' },
+    { id: '8', title: 'Unit Testing Fundamentals' },
+  ];
 
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.currentId = params.get('id');
+      if(this.currentId) {
+        this.setCurrentLesson(this.currentId);
+      }
+    })
   }
 
+  setCurrentLesson(id) {
+    this.currentLesson = this.lessons.find(lesson => lesson.id === id);
+  }
 }
